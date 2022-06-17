95555824baa372fb6f8afc77ee3a9405ebd6525b
kwesiansu16 <kwesiansu16@gmail.com>
2022-06-17 13:48:08 -0700

    router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-     hi everyone
-      
-    </div>
-  );
+  return <Switch>
+
+    <Route exact path="/">
+This is the home page
+    </Route>
+
+    <Route exact path="/starred">
+This is the starred page
+    </Route>
+  </Switch>
+  
 }
 
 export default App;
